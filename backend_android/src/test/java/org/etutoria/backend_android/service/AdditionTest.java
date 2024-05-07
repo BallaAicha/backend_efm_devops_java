4ed0bd37e9cff16e4db068aa76efd6e7737bc8bb
Ousmane <usmanembacke@gmail.com>
2024-05-07 04:19:50 +0100

    Test unitairev3

---
+++ backend_android/src/test/java/org/etutoria/backend_android/service/AdditionTest.java
@@ -0,0 +1,24 @@
+package org.etutoria.backend_android.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AdditionTest {
+
+    @Test
+    public void testAddition() {
+        // Arrange
+        int a = 5;
+        int b = 3;
+
+        // Act
+        int result = add(a, b);
+
+        // Assert
+        assertEquals(8, result);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
